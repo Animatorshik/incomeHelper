4e151775366b6f772f2a16ee603650547ea0e226
Vlad Tregubov <u.trehubau@gmail.com>
2023-05-08 01:46:00 +0200

    Hidden old months data by default

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import recordsReducer from '../features/records/recordsSlice';
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
         records: recordsReducer,
     },
     // middleware: [...getDefaultMiddleware(), saveStateMiddleware],
--- src/components/Records.tsx
+++ src/components/Records.tsx
@@ -4,6 +4,7 @@ import dayjs from 'dayjs';
 
 import { selectRecords } from '../features/records/recordsSlice';
 import Record from './Record';
+import Limits from './Limits';
 
 import {
     Paper,
@@ -13,8 +14,12 @@ import {
     TableContainer,
     TableHead,
     TableRow,
+    IconButton,
 } from '@mui/material';
-import Limits from './Limits';
+import {
+    KeyboardArrowDown,
+    KeyboardArrowUp,
+} from '@mui/icons-material';
 
 import { TRecord } from '../types/records';
 
@@ -23,6 +28,10 @@ type TSortedRecords = {
 }
 
 type TSplitFormat = 'YYYY' | 'YYYY-MM'
+
+type TShowByMonth = {
+    [key: string]: boolean,
+}
 
 const filteredRecords = (records: TRecord[]) => {
     return [...records].sort((a, b) => b.date - a.date);
@@ -50,63 +59,96 @@ export default function Records() {
     const records = useSelector<TRecord[]>(selectRecords);
     const [recordsByMonth, setRecordsByMonth] = useState<TSortedRecords>({});
     const [recordsByYear, setRecordsByYear] = useState<TSortedRecords>({});
+    const [showByMonth, setShowByMonth] = useState<TShowByMonth>({});
 
     useEffect(() => {
         setRecordsByMonth(splitRecordsBy(records, 'YYYY-MM'));
         setRecordsByYear(splitRecordsBy(records, 'YYYY'));
     }, [records]);
 
+    useEffect(() => {
+        if (Object.keys(showByMonth).length === 0) {
+            const newShowByMonth = {};
+            const keysArray = Object.keys(recordsByMonth);
+    
+            keysArray.map((key) => {
+                newShowByMonth[key] = false;
+                return false;
+            });
+    
+            setShowByMonth(newShowByMonth);
+        }
+    }, [recordsByMonth]);
+
+    const toggleMonth = (key: string) => {
+        const newShowByMonth = { ...showByMonth };
+        newShowByMonth[key] = !newShowByMonth[key];
+        setShowByMonth(newShowByMonth);
+    };
+
     return (
         <>
             {recordsByMonth && Object.keys(recordsByMonth).map((key) => (
-                <div key={key} className='mt-10'>
+                <div key={key} className='mt-0'>
                     {dayjs().format('YYYY-MM') === key
                         ? (
                             <Limits records={recordsByMonth[key]}/>
                         )
                         : (
-                            <div className='capitalize font-medium py-4'>
-                                {dayjs(key).format('MMMM YYYY')}
+                            <div className='capitalize font-medium py-2'>
+                                <IconButton onClick={() => toggleMonth(key)}>
+                                    {showByMonth[key]
+                                        ? <KeyboardArrowUp />
+                                        : <KeyboardArrowDown />
+                                    }
+                                </IconButton>
+                                <span className='ml-2'>
+                                    {dayjs(key).format('MMMM YYYY')}
+                                </span>
                                 <span className='text-slate-500 ml-5 lowercase text-sm'>
                                     {getSumForRecords(recordsByMonth[key])} zł
                                 </span>
                             </div>
                         )
                     }
-                    <TableContainer component={Paper}>
-                        <Table size="small" aria-label="a dense table">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>
-                                        <div className='w-20 md:w-32 leading-5'>Сумма перевода</div>
-                                    </TableCell>
-                                    <TableCell>
-                                        <div className='w-20 leading-5'>От кого</div>
-                                    </TableCell>
-                                    <TableCell>
-                                        <div className='w-20 leading-5'>Дата</div>
-                                    </TableCell>
-                                    <TableCell>
-                                        <div className='w-20 leading-5'>Получатель</div>
-                                    </TableCell>
-                                    <TableCell>
-                                        <div className='w-20 leading-5'>Сумма в zł</div>
-                                    </TableCell>
-                                    <TableCell>
-                                        <div className='w-20 leading-5'>Курс</div>
-                                    </TableCell>
-                                    <TableCell align='right'>
-                                        <div className='w-16 leading-5'>Действие</div>
-                                    </TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {recordsByMonth[key].map((record) => (
-                                    <Record key={record.id} {...record} />
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
+                    {(dayjs().format('YYYY-MM') === key || showByMonth[key]) &&
+                    <div className='mb-10'>
+                        <TableContainer component={Paper}>
+                            <Table size="small" aria-label="a dense table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>
+                                            <div className='w-20 md:w-32 leading-5'>Сумма перевода</div>
+                                        </TableCell>
+                                        <TableCell>
+                                            <div className='w-20 leading-5'>От кого</div>
+                                        </TableCell>
+                                        <TableCell>
+                                            <div className='w-20 leading-5'>Дата</div>
+                                        </TableCell>
+                                        <TableCell>
+                                            <div className='w-20 leading-5'>Получатель</div>
+                                        </TableCell>
+                                        <TableCell>
+                                            <div className='w-20 leading-5'>Сумма в zł</div>
+                                        </TableCell>
+                                        <TableCell>
+                                            <div className='w-20 leading-5'>Курс</div>
+                                        </TableCell>
+                                        <TableCell align='right'>
+                                            <div className='w-16 leading-5'>Действие</div>
+                                        </TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {recordsByMonth[key].map((record) => (
+                                        <Record key={record.id} {...record} />
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </div>
+                    }
                 </div>
             ))}
 
